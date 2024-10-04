842bad629cfcd764ab14532ac011f44b091b6c75
arturaff <arturars2022@gmail.com>
2024-10-04 20:58:02 +0500

    Первый пуш!

---
+++ app/src/main/java/ru/arturprgr/mywarmer/receiver/TempReceiver.kt
@@ -0,0 +1,13 @@
+package ru.arturprgr.mywarmer.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class TempReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("TempReceiver.onReceive() is not implemented")
+    }
+}
